4737d433918eced3f0bf0b1f0a6592b4c822678f
Vitaliy <vitaliy@MacBook-Pro-Vitaliy.local>
2018-03-10 12:09:37 +0200

    change name desc

--- app/model.js
+++ app/model.js
@@ -5,7 +5,7 @@ const eventSchema = new mongoose.Schema({
   title: { type: String },
   lng: { type: Number, required: true },
   lat: { type: Number, required: true },
-  desc: { type: String }
+  description: { type: String }
 })
 
 module.exports = mongoose.model('Event', eventSchema)
